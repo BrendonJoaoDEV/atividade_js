ecddd12a2d01c7a3d8ff431aa924f9057b5ba74d
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-06-26 21:35:22 -0300

    Feat: Exercício B: Maior, Menor ou Igual - Continuação

--- atividade004_praticando_condicionais/js/maior_menor_igual.js
+++ atividade004_praticando_condicionais/js/maior_menor_igual.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function verificarMaiorMenorIgual (numeros) {
+    const entrada1 = document.getElementById('numero1');
+    const entrada2 = document.getElementById('numero2');
+    const entrada3 = document.getElementById('numero3');
+
+
+    document.getElementById('btnVerificar').addEventListener('click', function () {
+        let numeros = [parseInt(entrada1.value), parseInt(entrada2.value), parseInt(entrada3.value)];
+
+        let maior = Math.max(numeros);
+        let menor = Math.min(numeros);
         
-    }
+        console.log(Math.max(numeros));
+        console.log(Math.min(numeros));
+        console.log(numeros);
+    })
 })
+
+    // function verificarMaiorMenorIgual (numeros) {
+    //     let maior = Math.max(numeros);
+    //     let menor = Math.min(numeros);
+
+    //     let max_min = [maior, menor];
+
+    //     let numeros_restantes = numeros.filter(function (numero) {
+    //         return !(numero in max_min); 
+    //     });
+
+    //     return maior
+    // }
